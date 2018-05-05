b3bfcd1d2ac3ff9f9745d892e12daa5a64a3e90f
Muhammed <mselrakabawi@gmail.com>
2018-05-05 21:41:53 +0200

    Inline documentation

--- icgr.py
+++ icgr.py
@@ -1,36 +1,46 @@
-# Open The file containing input
-file = open('input.txt', 'r')
-seq = file.read()
-
-i_value = 0
-alpha_x = 0
-alpha_y = 0
+#############################################################################
+# Author: Muhammed S. ElRakabawi (elrakabawi.github.io)
+# Paper: Yin, C. (2017). Encoding DNA sequences by integer chaos game representation
+# License: MIT
+#############################################################################
 
 
-def clean_file(this_file):
-    this_file.seek(0)
-    this_file.truncate()
-    this_file.seek(0)
+file = open('input.txt', 'r')                                               # Open The file containing input
+seq = file.read()                                                           # Reading DNA sequence
+i_value = 0                                                                 # nucleotide index (position)
+alpha_x = 0                                                                 # Alpha (i,x)
+alpha_y = 0                                                                 # Alpha (i,y)
+seq_len = len(seq)                                                          # Sequence length
+arr = []                                                                    # List of lists [(i,Xi,Yi)..(n, X, Yn)]
+cgr_coordinate = {'A': (1, 1), 'G': (1, -1), 'T': (-1, 1), 'C': (-1, -1)}   # Nucleotide mapping to coordinates
 
+
+#############################################################################
+# Functions
+#############################################################################
+
+
+def clean_file(this_file):                                                  # Truncating function for cleaning the file
+    this_file.seek(0)                                                       # Point cursor to first line
+    this_file.truncate()                                                    # Truncate file
+    this_file.seek(0)                                                       # Return cursor to first line
     return this_file
 
 
-answer_file = open('output.txt', 'a')
-answer_file = clean_file(answer_file)
+#############################################################################
+# File Handling and Core logic
+#############################################################################
 
-for n in seq:
-    if n == 'A':
-        alpha_x = 1
-        alpha_y = 1
-    elif n == 'T':
-        alpha_x = -1
-        alpha_y = 1
-    elif n == 'C':
-        alpha_x = -1
-        alpha_y = -1
-    elif n == 'G' :
-        alpha_x = 1
-        alpha_y = -1
 
-    cor = str(n) + 'i is: ' + str(i_value) + ', x: ' + str(alpha_x) + ', y: ' + str(alpha_y) + '\n'
-    answer_file.write(cor)
+answer_file = open('output.txt', 'a')                                       # Open the file containing output
+answer_file = clean_file(answer_file)                                       # Truncate the file
+
+for n in seq:                                                               # For every nucleotide in sequence
+    i_value = i_value + 1                                                   # increment i --> n
+    alpha_x = cgr_coordinate[n][0]                                          # alpha_x = Nucleotide Xi coordinate
+    alpha_y = cgr_coordinate[n][1]                                          # alpha_y = Nucleotide Yi coordinate
+    arr.append([i_value, alpha_x, alpha_y])                                 # Append (i, Xi, Yi) to the list
+
+answer_file.write(str(arr))                                                 # Write the list to the output file
+print('\n'.join([''.join(['{:4}'.format(item) for item in row])             # Matrix format for the list [debugging]
+                 for row in arr]))
