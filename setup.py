cea0db6208509d24e21a4ce0d9d75eab7610edf1
Muhammed <mselrakabawi@gmail.com>
2018-05-12 18:48:33 +0200

    Packaging for PyPi v1.0.0

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='icgr-py',
+    version='1.0.0',
+    packages=['icgr-py'],
+    url='https://github.com/ElRakabawi/icgr-py',
+    license='MIT License',
+    author='elrakabawi',
+    author_email='elrakabawi@lamarkaz.com',
+    description='Integer chaos game representation encoder/decoder in python.'
+)
